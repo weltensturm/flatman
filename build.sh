f1f592e41e4527c1ceba3abbd5c7c52f261371e0
Andrej <xentec@users.noreply.github.com>
2016-08-30 17:01:40 +0200

    Silence error message about removing dub.selections.json

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ set -e
 
 for part in "${parts[@]}"; do
 	echo "Building $part"
-	(cd "$part"; $(rm dub.selections.json || true); dub build > /dev/null)
+	(cd "$part"; $(rm dub.selections.json 2&>/dev/null || true); dub build > /dev/null)
 done
